b0d094a9462a24b9a1078775c4a41b7481d6354d
vitalis-virtus <vshayuk@gmail.com>
2023-05-18 13:04:32 +0300

    Generate an Ether Wallet

---
+++ wallet/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
+
+	"log"
+)
+
+func main() {
+	pvk, err := crypto.GenerateKey() // we generate privateKey
+	if err != nil {
+		log.Fatal(err)
+	}
+	pData := crypto.FromECDSA(pvk)
+	fmt.Println(hexutil.Encode(pData))
+
+	pubData := crypto.FromECDSAPub(&pvk.PublicKey)
+	fmt.Println(hexutil.Encode(pubData))
+
+	fmt.Println(crypto.PubkeyToAddress(pvk.PublicKey).Hex())
+}
