08edb5fb0a2b76360c9977e8b40a11a3cda7e7fe
vitalis-virtus <vshayuk@gmail.com>
2023-05-18 21:22:57 +0300

    add authorization modifier in todo smart contract

--- smart-contracts/todo.sol
+++ smart-contracts/todo.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract Todo {
+    address public owner;
     Task[] tasks;
 
     struct Task {
@@ -8,25 +9,35 @@ contract Todo {
         bool status;
     }
 
-    constructor() {}
+    constructor() {
+        owner = msg.sender;
+    }
 
-    function add(string memory _content) public {
+    modifier isOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+
+    function add(string memory _content) public isOwner {
         tasks.push(Task(_content, false));
     }
 
-    function get(uint256 _id) public view returns (Task memory) {
+    function get(uint256 _id) public view isOwner returns (Task memory) {
         return tasks[_id];
     }
 
-    function list() public view returns (Task[] memory) {
+    function list() public view isOwner returns (Task[] memory) {
         return tasks;
     }
 
-    function update(uint256 _id, string memory _content) public {
+    function update(uint256 _id, string memory _content) public isOwner {
         tasks[_id].content = _content;
     }
 
-    function remove(uint _id) public {
-        delete tasks[_id];
+    function remove(uint256 _id) public isOwner {
+        for (uint256 i = _id; i < tasks.length - 1; i++) {
+            tasks[i] = tasks[i + 1];
+        }
+        tasks.pop();
     }
 }
